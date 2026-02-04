34af06c13811e590b97a95061863a3c692426c6c
danielyu128 <danielyu128@gmail.com>
2026-02-03 20:51:28 -0500

    Add leftBar.js and styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 // index.js
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import loadLeftBar from "./leftBar.js";
 
 console.log(greeting);
+
+const content = document.createElement("div");
+
+const {leftBar} = loadLeftBar();
+
+
---
+++ src/leftBar.js
@@ -0,0 +1,43 @@
+export default function loadLeftBar() {
+
+    //Left Menu Bar Container
+    const leftBar = document.createElement("div");
+    leftBar.classList.add("leftBar");
+
+    const leftBarTop = document.createElement("div");
+    leftBarTop.classList.add("leftBarTop");
+
+    const leftBarHeader = document.createElement("div");
+    leftBarHeader.classList.add("leftBarHeader");
+
+    const leftBarCategories = document.createElement("div");
+    leftBarCategories.classList.add("leftBarCategories");
+
+    const leftBarCategory1 = document.createElement("div");
+    leftBarCategory1.classList.add("leftBarCategory1");
+
+    const leftBarCategory1Img = document.createElement("div");
+    leftBarCategory1Img.classList.add("leftBarCategory1Img");
+
+    const leftBarCategory1Title = document.createElement("div");
+    leftBarCategory1Title.classList.add("leftBarCategory1Title");
+    leftBarCategory1Title.textContent = "All Tasks"
+
+    leftBarCategory1.append(leftBarCategory1Img,leftBarCategory1Title);
+
+    leftBarCategories.append(leftBarCategory1);
+
+    leftBarTop.append(leftBarHeader,leftBarCategories);
+
+
+    const leftBarButton = document.createElement("button");
+    leftBarButton.classList.add("leftBarButton");
+    leftBarButton.textContent = "+ New Category"
+
+
+    leftBar.append(leftBarTop,leftBarButton)
+
+    return {leftBar};
+
+
+}
